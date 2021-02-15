3b32433cc82c0ee00c7926a9dd354057dee97f12
czming <zchng3@gatech.edu>
2021-02-15 13:00:42 +0800

    Added bounding box and left/right motion tracking

--- gesture_recognition_functions.py
+++ gesture_recognition_functions.py
@@ -2,44 +2,112 @@ import math
 import numpy as np
 import cv2
 
-def get_distance(point1, point2, image_shape):
+
+def get_distance(point1, point2, image_shape=None):
+    """
+    get pixel distance between two points
+    :param point1: (x, y) of first point, where x and y are ratios of the point's location to respective image param
+    :param point2: (x, y) of second point, where x and y are ratios of the point's location to respective image param
+    :param image_shape: shape of the image
+    :return: pixel distance between the two points
+    """
+
+    if image_shape == None:
+        # no image shape passed in, assume to scale
+        image_shape = (1, 1)
     # get euclidean distance between point1 (x1, y1) and point2 (x2, y2)
-    return math.sqrt(((point1[0]  - point2[0]) * image_shape[1]) ** 2 + ((point1[1] - point2[1]) * image_shape[0]) ** 2)
+    return math.sqrt(((point1[0] - point2[0]) * image_shape[1]) ** 2 + ((point1[1] - point2[1]) * image_shape[0]) ** 2)
 
 
 def improved_gesture_recognition(landmarks, handedness, image):
     """
     Gesture recognition method based on detecting whether each finger is open or closed.
-
     Determines if thumb is open or closed based on relative proximity between tip of thumb and base of palm and
     proximity between base of pinky and base of palm
-
     Determines if other fingers are open based on relatively proximity between each finger landmark and the base
     (a straight finger should have increasing keypoints further from the base of the palm)
-    :param landmarks: array of 21 landmarks, each landmark denoted by [x,y] coordinates
+    :param landmarks: (x,y) of each landmark in an array as a ratio of image's shape
     :param handedness: left or right hand
     :param image: image that is being detected from
     :return: array of 5 elements denoting if each finger is open, 0 = thumb, 1 = index and so on
     """
     fingers = [[np.array(landmarks[i]) for i in range(j * 4 + 1, j * 4 + 5)] for j in range(5)]
     fingers_open = [0] * 5
-    #thumb detection, tip of thumb is closer to base of pinky than base of second finger
+    # thumb detection, tip of thumb is closer to base of pinky than base of second finger
     # fingers_open[0] = get_distance(fingers[0][-1], fingers[1][0], image.shape) < \
     #                   get_distance(fingers[0][-1], fingers[4][0], image.shape)
 
-    #thumb detection, base of palm is closer to base of pinky than tip of thumb to base of pinky
+    # thumb detection, base of palm is closer to base of pinky than tip of thumb to base of pinky
     fingers_open[0] = get_distance(landmarks[0], fingers[4][0], image.shape) < \
                       get_distance(fingers[0][-1], fingers[4][0], image.shape)
 
     for finger_index in range(1, 5):
         finger = fingers[finger_index]
-        #finger is open if the tip is further from the base of the palm than the base of the finger to the base of the
+        # finger is open if the tip is further from the base of the palm than the base of the finger to the base of the
         # palm
         finger_open = True
         for i in range(len(finger) - 1):
-            #check all points in the finger, if the tip is closer to the base of the palm than any finger, it's closed
+            # check all points in the finger, if the tip is closer to the base of the palm than any finger, it's closed
             if get_distance(finger[len(finger) - 1], landmarks[0], image.shape) < \
                     get_distance(finger[i], landmarks[0], image.shape):
                 finger_open = False
         fingers_open[finger_index] = finger_open
     return fingers_open
+
+
+def hand_pos(landmarks, image):
+    """
+    outputs the average (x, y) coordinate of the landmarks to indicate the hand position (which is denoted by center
+    of palm
+    :param landmarks: (x,y) of each landmark in an array as a ratio of image's shape
+    :return: (x, y) coordinates of hand
+    """
+    if len(landmarks) < 21:
+        # need at least 21 for a hand
+        return ()
+
+    # center of palm appears to be a better indicator for the location of the hand (more mass concentrated at that point)
+    fingers = [[np.array(landmarks[i]) for i in range(j * 4 + 1, j * 4 + 5)] for j in range(5)]
+    # base of the palm
+    total_x = landmarks[0][0] * image.shape[1]
+    total_y = landmarks[0][1] * image.shape[0]
+    for finger in fingers:
+        # look at base of the finger
+        curr = finger[0]
+        total_x += (curr[0] * image.shape[1])
+        total_y += (curr[1] * image.shape[0])
+    return (total_x / 6, total_y / 6)
+
+def hand_bounding_box(landmarks, image):
+    """
+    gets the bounding box of the hand and the side of the bounding box
+    :param landmarks: (x,y) of each landmark in an array as a ratio of image's shape
+    :param image: image the landmarks are on (used for image.shape)
+    :return: (bounding box of hand in image (top left at index 0, and clockwise from there),
+     size of the bounding box (height, width))
+    """
+
+    left = image.shape[1]
+    right = -1
+    top = image.shape[0]
+    bottom = -1
+    for (x, y) in landmarks:
+        #adjust for pixel values
+        x = x * image.shape[1]
+        y = y * image.shape[0]
+        if x < left:
+            # point further left than current left
+            left = x
+        if x > right:
+            # point further right than current right
+            right = x
+        if y < top:
+            #point that is higher than top
+            top = y
+        if y > bottom:
+            bottom = y
+
+    #add a little buffer for the rest of the finger (the landmarks aren't exactly at the tip)
+    top, bottom, left, right = int(top - 20), int(bottom + 20), int(left - 20), int(right + 20)
+    # return points (top left and clockwise from there, (x, y) for points), size (height, width)
+    return ((left, top), (right, top), (right, bottom), (left, bottom)), (bottom - top, right - left)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 from gesture_recognition_functions import *
 from google.protobuf.json_format import MessageToDict
+import time
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -9,6 +10,15 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.6, max_num_hands=1)
 #live video feed from camera 0
 cap = cv2.VideoCapture(0)
+
+MOVEMENT_MOE = 5
+REFRESH_TIME = 0.05
+
+prev_hand_loc = ()
+curr_hand_loc = ()
+prev_time = time.time()
+
+
 while cap.isOpened():
 	success, image = cap.read()
 	if not success:
@@ -33,14 +43,44 @@ while cap.isOpened():
 		#get which hand we're looking at (look at the first hand that is detected)
 		handedness = MessageToDict(results.multi_handedness[0])['classification'][0]['label']
 		for hand_landmarks in results.multi_hand_landmarks:
+			#look only at the first hand detected if there are multiple hands
 			for i in hand_landmarks.landmark[:21]:
 				first_hand_points.append([i.x, i.y])
 				#image = cv2.circle(image, (int(i.x * image.shape[1]), int(i.y * image.shape[0])), 2, (0, 0, 255), 3)
 				mp_drawing.draw_landmarks(
 				   image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-	cv2.imshow('MediaPipe Hands', image)
 	if len(first_hand_points) >= 21:
-		print ("\r", improved_gesture_recognition(first_hand_points, handedness, image), handedness, end = "")
+		curr_time = time.time()
+		time_change = curr_time - prev_time
+
+		# find current location of hand
+		curr_hand_loc = hand_pos(first_hand_points, image)
+		cv2.circle(image, (int(curr_hand_loc[0]), int(curr_hand_loc[1])), 3, (255, 0, 255), 3)
+
+		# find bounding box of hand
+		bounding_box, bounding_box_size = hand_bounding_box(first_hand_points, image)
+		for point_index in range(len(bounding_box)):
+			#use the %len(bounding_box) to wrap around back to the start
+			cv2.line(image, bounding_box[point_index], bounding_box[(point_index + 1) % len(bounding_box)],
+							 (0, 255, 0), 4)
+
+		# motion related stuff that is checked eveyr fixed period (otherwise different frame refresh rates lead to
+		# inconsistencies in measuring movement in pixels)
+		if (time_change > REFRESH_TIME):
+			# checks when the time_change is greater than the REFRESH TIME
+
+			print ("\r", improved_gesture_recognition(first_hand_points, handedness, image), handedness, end = "")
+			if prev_hand_loc != () and (curr_hand_loc[0] > prev_hand_loc[0] + MOVEMENT_MOE * time_change / REFRESH_TIME):
+				# curr_hand is more than MOVEMENT_MOE pixels to the right of prev_hand_loc
+				print("Moving right", end="")
+
+			elif prev_hand_loc != () and (curr_hand_loc[0] < prev_hand_loc[0] - MOVEMENT_MOE * time_change / REFRESH_TIME):
+				print("Moving left", end=" ")
+
+			prev_hand_loc = curr_hand_loc
+			prev_time = curr_time
+
+	cv2.imshow('MediaPipe Hands', image)
 	if cv2.waitKey(5) & 0xFF == 27:
 		break
 hands.close()
