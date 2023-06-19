5e2fede2a2964a37475d1f37ebd372d396c7f93f
Shivang Chopra <shivangchopra11@gmail.com>
2023-06-19 05:31:13 -0700

    Pipeline complete

--- scripts/deep_learning/pipeline.sh
+++ scripts/deep_learning/pipeline.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mkdir ../../data/videos
-python utils/download_videos.py 
-python utils/extract_frames.py
+python dl_utils/download_videos.py 
+python dl_utils/extract_frames.py
+python prep_triplet_data.py
+python train_triplet.py
