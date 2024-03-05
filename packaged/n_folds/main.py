84ed5cea5ce5b3ca5c4887bef31de9a1aaee28d3
Ananay-22 <guptavananay@gmail.com>
2024-03-05 14:19:49 -0500

    added subprocess error output printer

--- packaged/n_folds/main.py
+++ packaged/n_folds/main.py
@@ -3,18 +3,23 @@ import subprocess
 from service import Service
 
 def run_htk_on_dirs(_dir, num_folds, split_ratio=0.7):
-    return subprocess.check_output(["/bin/bash", "./scripts/n_fold.sh", "-s", f"{split_ratio}", "-d", f"{_dir}/data", "-n", f"{num_folds}"])
+    try:
+        x = subprocess.check_output(["/bin/bash", "./scripts/n_fold.sh", "-s", f"{split_ratio}", "-d", f"{_dir}/data", "-n", f"{num_folds}"])
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        x = -1
+    return x
 
 service = Service(
     "htk",
     lambda id, _dir, num_folds, split_ratio: str((id, subprocess.check_output(["/bin/bash", "./symbiosis/scripts/n_folds.sh", "-s", f"{split_ratio}", "-d", f"{_dir}/data", "-n", f"{num_folds}"]))),
     {
-		'form': ['id', '_dir', 'num_folds', 'split_ratio'],
-	}
+        'form': ['id', '_dir', 'num_folds', 'split_ratio'],
+    }
 ).create_service(init_cors=True)
 
 if __name__ == '__main__':
-	service.run(
-		host=os.environ.get("HOST", "0.0.0.0"),
-		port=os.environ.get("PORT", 5000)
-	)
+    service.run(
+        host=os.environ.get("HOST", "0.0.0.0"),
+        port=os.environ.get("PORT", 5000)
+    )
